cb39e241a3410b9f92ac320de88dd677bd7db154
Lauro Lyra <laurolyra@gmail.com>
2024-02-25 20:04:51 -0300

    Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import buildProvidersTree from '@/components/ProvidersTree';
+import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} container mx-auto bg-primary-dark`}>
         <ProvidersTree>
-          <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">
+            <Header />
+            {children}
+          </main>
         </ProvidersTree>
       </body>
     </html>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h2>Header Component</h2>
+    </div>
+  );
+}
