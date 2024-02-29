8b06b7d8f02f277854306118b8540b0ba7b7c865
Lauro Lyra <laurolyra@gmail.com>
2024-02-28 21:21:59 -0300

    header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
 export default function Header() {
   return (
-    <div>
-      <h2>Header Component</h2>
-    </div>
+    <header>
+      <h1 className="text-center text-2xl">Lauro Lyra</h1>
+      <h2 className="text-center text-xl">Learn. Create. Teach.</h2>
+      <div className="flex justify-between items-center">
+        <p>route1</p>
+        <p>route1</p>
+        <p>route1</p>
+        <p>route1</p>
+      </div>
+    </header>
   );
 }
