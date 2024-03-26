65e3f89c39d1f6dea63f01676cc8c17d4371b887
Lauro Lyra <laurolyra@gmail.com>
2024-03-25 22:44:54 -0300

    local fetching ok

---
+++ src/app/services/getPosts.ts
@@ -0,0 +1,29 @@
+export type Post = {
+  title: string;
+  headline: string;
+  slug: string;
+  date: string;
+  content: string;
+};
+
+export async function getPosts(): Promise<Post[]> {
+  const response = await fetch(process.env.HYGRAPH_ENDPOINT as string, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `query {
+        posts {
+          title
+          headline
+          slug
+          date
+          content
+        }
+      }`,
+    }),
+  });
+  const json = await response.json();
+  return <Post[]>json.data.posts;
+}
