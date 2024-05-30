80f73729c298c744d3c51bf536bc34f60444ae71
Lauro Lyra <laurolyra@gmail.com>
2024-05-30 11:20:17 -0300

    DynamicTag component

--- src/components/BlogPostsContainer.tsx
+++ src/components/BlogPostsContainer.tsx
@@ -1,26 +1,13 @@
 import { Post } from '@/services/getPosts';
 import ReactMarkdown from 'react-markdown';
-import React, { FC, HTMLAttributes } from 'react';
+import React from 'react';
 import remarkGfm from 'remark-gfm';
 import Image from 'next/image';
-
-interface ExtraProps {
-  node?: any;
-}
-
-type H1Props = HTMLAttributes<HTMLHeadingElement> & ExtraProps;
+import DynamicTag from './DynamicTag';
 
 const BlogPostsContainer = async ({ post }: { post: Post }) => {
   const { title, headline, slug, date, content } = post;
-  const H1:FC<H1Props> = (props) => {
-    const { node, ...rest } = props;
-    return <h1 className="text-2xl" {...rest} />;
-  }
-  const H2 = ({ children }) => <h2 className="text-xl">{children}</h2>;
-  const H3 = ({ children }) => <h3 className="text-lg">{children}</h3>;
-  const P = ({ children }) => (
-    <p className="text-base bg-yellow-400">{children}</p>
-  );
+
   return (
     <section key={slug}>
       <h2>Container</h2>
@@ -31,21 +18,47 @@ const BlogPostsContainer = async ({ post }: { post: Post }) => {
       <div>
         <ReactMarkdown
           components={{
-            h1: H1,
-            h2: H2,
-            h3: H3,
-            p(props) {
-              const { node, ...rest } = props;
-              return <p style={{ color: 'red' }} {...rest} />;
-            },
-            code(props) {
-              const { node, ...rest } = props;
-              return <code style={{ color: 'green' }} {...rest} />;
-            },
-            pre(props) {
-              const { node, ...rest } = props;
-              return <pre style={{ backgroundColor: 'blue' }} {...rest} />;
-            },
+            h1: (props) => (
+              <DynamicTag
+                as="h1"
+                className="text-3xl bg-slate-500"
+                {...props}
+              />
+            ),
+            h2: (props) => (
+              <DynamicTag as="h1" className="text-2xl" {...props} />
+            ),
+            h3: (props) => (
+              <DynamicTag as="h1" className="text-xl" {...props} />
+            ),
+            p: (props) => (
+              <DynamicTag
+                as="p"
+                className="text-base text-red-300"
+                {...props}
+              />
+            ),
+            em: (props) => (
+              <DynamicTag
+                as="em"
+                className="text-base text-yellow-500"
+                {...props}
+              />
+            ),
+            code: (props) => (
+              <DynamicTag
+                as="code"
+                className="text-green-400 bg-black"
+                {...props}
+              />
+            ),
+            pre: (props) => (
+              <DynamicTag
+                as="pre"
+                className="bg-blue-500 *:bg-inherit"
+                {...props}
+              />
+            ),
             img: (img) => {
               return (
                 <Image
@@ -63,7 +76,6 @@ const BlogPostsContainer = async ({ post }: { post: Post }) => {
           {content}
         </ReactMarkdown>
       </div>
-      {/* <p>{content}</p> */}
     </section>
   );
 };
